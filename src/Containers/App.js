3978930fd79137717ad9194881a377a40bc5cbe7
Marlin <blutuu1@gmail.com>
2022-08-10 12:52:16 -0400

    Prepping for PWA integration

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,55 +1,53 @@
-import React, {useState, useEffect, Component} from 'react';
-import { connect } from 'react-redux'; 
-import './App.css';
-import CardList from '../Components/CardList';
+import React, { useState, useEffect, Component } from "react";
+import { connect } from "react-redux";
+import "./App.css";
+import CardList from "../Components/CardList";
 import SearchBox from "../Components/SearchBox";
-import Scroll from '../Components/Scroll';
-import ErrorBoundary from '../Components/ErrorBoundary';
-import { setSearchField, requestRobots } from '../Redux/actions';
+import Scroll from "../Components/Scroll";
+import ErrorBoundary from "../Components/ErrorBoundary";
+import { setSearchField, requestRobots } from "../Redux/actions";
 
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     searchField: state.searchRobots.searchField,
     robots: state.requestRobots.robots,
     isPending: state.requestRobots.isPending,
-    error: state.requestRobots.error
-  }
-}
+    error: state.requestRobots.error,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => requestRobots(dispatch)
-  }
-}
+    onRequestRobots: () => requestRobots(dispatch),
+  };
+};
 
 class App extends Component {
-  
-
   componentDidMount() {
     this.props.onRequestRobots();
   }
 
   render() {
     // const { robots } = this.state;
-    const { searchField, onSearchChange, robots, isPending, error } = this.props;
-    const filteredRobots = robots.filter(robot => {
+    const { searchField, onSearchChange, robots, isPending, error } =
+      this.props;
+    const filteredRobots = robots.filter((robot) => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    return isPending ?
-    <h1>LOADING...</h1> :
-    (
-        <div className="App">
-          <h1 className="f2">Robofriends</h1>
-          <SearchBox searchChange={ onSearchChange } />
-          <Scroll>
-            <ErrorBoundary>
-              <CardList robots={ filteredRobots }/>
-            </ErrorBoundary>
-          </Scroll>
-        </div>
+    return isPending ? (
+      <h1>LOADING...</h1>
+    ) : (
+      <div className="App">
+        <h1 className="f2">Robofriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'; 
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger'; 
-import thunkMiddleware from 'redux-thunk';
-import './index.css';
-import 'tachyons';
-import App from './Containers/App';
-import { searchRobots, requestRobots } from './Redux/reducers';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM, { hydrate, render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import "./index.css";
+import "tachyons";
+import App from "./Containers/App";
+import { searchRobots, requestRobots } from "./Redux/reducers";
+import reportWebVitals from "./reportWebVitals";
 
 const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots });
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  rootElement
 );
+
+// if (rootElement.hasChildNodes()) {
+//   hydrate(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>,
+//     rootElement
+//   );
+// } else {
+//   render(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>,
+//     rootElement
+//   );
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
