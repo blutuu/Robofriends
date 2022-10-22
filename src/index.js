9d557215344772839a5cf1ba6474d729f64f6fb9
Marlin <blutuu1@gmail.com>
2022-10-21 21:31:31 -0400

    Added offline support

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM, { hydrate, render } from "react-dom";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
-import "./index.css";
 import "tachyons";
+import "./index.css";
 import App from "./Containers/App";
 import { searchRobots, requestRobots } from "./Redux/reducers";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 const logger = createLogger();
@@ -25,21 +26,10 @@ ReactDOM.render(
   rootElement
 );
 
-// if (rootElement.hasChildNodes()) {
-//   hydrate(
-//     <Provider store={store}>
-//       <App />
-//     </Provider>,
-//     rootElement
-//   );
-// } else {
-//   render(
-//     <Provider store={store}>
-//       <App />
-//     </Provider>,
-//     rootElement
-//   );
-// }
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
